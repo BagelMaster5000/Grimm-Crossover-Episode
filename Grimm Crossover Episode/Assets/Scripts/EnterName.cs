17ba92da740f43c79dbcf0a88a3a97f96832b5cb
Sammy Mahmoudi <bagelmaster5001@gmail.com>
2020-10-01 12:39:24 -0500

    Added name input scene. Not connected to anything yet

---
+++ Grimm Crossover Episode/Assets/Scripts/EnterName.cs
@@ -0,0 +1,20 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnterName : MonoBehaviour
+{
+    public TMP_InputField nameInput;
+    public Button startButton;
+
+    private void Update() {
+        print(nameInput.textComponent.text);
+        startButton.interactable = nameInput.textComponent.text.Length > 1; }
+
+    public void StartGame()
+    {
+        GlobalVariables.playerName = nameInput.textComponent.text;
+        print("Start game");
+        // FIXME load first scene
+    }
+}

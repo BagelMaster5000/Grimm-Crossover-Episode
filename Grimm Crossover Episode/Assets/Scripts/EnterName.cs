5839b6d4f6bf605905390cbfa965cf00614223a7
Sammy Mahmoudi <bagelmaster5001@gmail.com>
2020-10-03 14:25:25 -0500

    Everything should be fixed

--- Grimm Crossover Episode/Assets/Scripts/EnterName.cs
+++ Grimm Crossover Episode/Assets/Scripts/EnterName.cs
@@ -6,6 +6,7 @@ public class EnterName : MonoBehaviour
 {
     public TMP_InputField nameInput;
     public WebGLNativeInputField webGLNameInput;
+    public GameObject webGLEnterNameText;
     public Button startButton;
 
     private void Start()
@@ -20,11 +21,25 @@ public class EnterName : MonoBehaviour
 #endif
     }
 
-    private void Update() { startButton.interactable = nameInput.textComponent.text.Length > 1; }
+    private void Update()
+    {
+#if UNITY_WEBGL
+        startButton.interactable = webGLNameInput.text.Length > 1;
+        webGLEnterNameText.SetActive(webGLNameInput.text.Length <= 1);
+#endif
+#if !UNITY_WEBGL
+        startButton.interactable = nameInput.textComponent.text.Length > 1;
+#endif
+    }
 
     public void StartGame()
     {
+#if UNITY_WEBGL
+        GlobalVariables.playerName = webGLNameInput.text;
+#endif
+#if !UNITY_WEBGL
         GlobalVariables.playerName = nameInput.textComponent.text;
+#endif
         FindObjectOfType<NextSceneFader>().FadeToNextScene("Story", true);
     }
 }
